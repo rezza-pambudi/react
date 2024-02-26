3d974c26fba4fc94e0038a807304932f7dbc9355
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-02-26 16:46:11 +0700

    react project

---
+++ myreactpro/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    );
+  }
+}
+
+function ButtonBlack() {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+}
+
+const ButtonRed = () => {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+};
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <ButtonBlack></ButtonBlack>
+        <ButtonRed></ButtonRed>
+      </div>
+    </div>
+  );
+}
+
+export default App;
