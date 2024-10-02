ffa431b385735ead083328212694b7bdbdb71d09
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-10-02 10:09:20 +0700

    kenapa-koding

---
+++ kenapa-coding/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element = document.getElementById("root");
+const root = ReactDOM.createRoot(element);
+
+const App = () => {
+    return <h1>Woooyyyy</h1>;
+};
+
+root.render(<App />);
