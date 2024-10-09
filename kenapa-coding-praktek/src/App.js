7ed33a6c1af51df368277c269d65c826fd9b0bac
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-10-09 14:25:55 +0700

    data

--- kenapa-coding-praktek/src/App.js
+++ kenapa-coding-praktek/src/App.js
@@ -1,21 +1,28 @@
 import "./App.css";
-
-const ProductCard = () => {
-  return (
-    <div className="card">
-      <img style={{ width: "100%", height: "200px", borderRadius: "10px 10px 0 0" }} src="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" alt="" />
-      <div className="container">
-        <h4><b>Toyota</b></h4>
-        <p>Raize seri terbaru dengan banyak fitur</p>
-      </div>
-    </div>
-  )
-}
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
-    <div className="cards"><ProductCard /></div>
-  ) ;
+    <>
+      <div className="cards">
+        <ProductCard
+          nama="Ford"
+          deskripsi="Mobil ford adalah mobil yang sangat bagus"
+          imageURL="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        />
+        <ProductCard
+          nama="Ford"
+          deskripsi="Mobil ford adalah mobil yang sangat bagus"
+          imageURL="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        />
+        <ProductCard
+          nama="Ford"
+          deskripsi="Mobil ford adalah mobil yang sangat bagus"
+          imageURL="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- kenapa-coding/src/App.js
+++ kenapa-coding/src/App.js
@@ -6,11 +6,20 @@ import Perkenalan from "./components/Perkenalan";
 // };
 
 const App = () => {
+    const datas = [
+        {name: "Aldi", age: 20, hobby: "membaca"},
+        {name: "Rezza", age: 30, hobby: "tidur"},
+        {name: "David", age: 20, hobby: "menulis"},
+        {name: "Lia", age: 20, hobby: "mengoding"},
+    ]
   return (
     <div>
       {/* <Button title="Click me" /> */}
-      <Perkenalan name="Aldi" age="20" hobby="membaca" />
-      <Perkenalan name="Rezza" age="30" hobby="tidur" />
+      {/* <Perkenalan name="Aldi" age="20" hobby="membaca" />
+      <Perkenalan name="Rezza" age="30" hobby="tidur" /> */}
+      {datas.map((data, index) => {
+        return <Perkenalan key={index} name={data.name} age={data.age} hobby={data.hobby} />
+      })}
     </div>
   );
 };
