a1b32304068956bfe8d2b89252d8cabb4bdba6be
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-10-16 16:58:18 +0700

    toko

--- tokoonline/website-admin/app/(root)/page.tsx
+++ tokoonline/website-admin/app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button size="lg" variant="destructive">Click me</Button>
+      <UserButton />
     </div>
   );
 }
