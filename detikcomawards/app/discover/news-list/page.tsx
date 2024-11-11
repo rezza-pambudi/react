59c79499d779f183f3c3a51b02d68f4e596b0a0b
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-11-11 13:18:28 +0700

    Update page.tsx

--- detikcomawards/app/discover/news-list/page.tsx
+++ detikcomawards/app/discover/news-list/page.tsx
@@ -16,7 +16,12 @@ export default function sectionNewsList() {
     fetchData();
   }, []);
 
-  type products = { id: number; image: number; title: string; description: string };
+  type products = {
+    id: number;
+    image: number;
+    title: string;
+    description: string;
+  };
 
   return (
     <>
@@ -25,12 +30,14 @@ export default function sectionNewsList() {
           <div className="grid grid-cols-12 gap-4">
             <div className="col-span-12 grid items-center">
               <Tabs defaultValue="news" className="w-full items-center">
-                <TabsList className="mx-auto">
-                  <TabsTrigger value="news">News</TabsTrigger>
-                  <TabsTrigger value="photo">Photo</TabsTrigger>
-                  <TabsTrigger value="video">Video</TabsTrigger>
-                </TabsList>
-                <TabsContent value="news">
+                <div className="flex w-full">
+                  <TabsList className="mx-auto md:max-w-[320px] bg-gradient-to-r from-[#323232] to-[#000000] rounded-[12px] h-auto p-0">
+                    <TabsTrigger value="news" className="hs-tab-active:bg-[#ffffff33] hs-tab-active:text-white hs-tab-active:font-semibold py-1 px-4 inline-flex items-center gap-2 bg-transparent text-[14px] font-semibold text-center text-gray-500 rounded-[12px] hover:text-[#80849f] dark:hover:text-gray-400 w-1/3 justify-center">News</TabsTrigger>
+                    <TabsTrigger value="photo" className="hs-tab-active:bg-[#ffffff33] hs-tab-active:text-white hs-tab-active:font-semibold py-1 px-4 inline-flex items-center gap-2 bg-transparent text-[14px] font-semibold text-center text-gray-500 rounded-[12px] hover:text-[#80849f] dark:hover:text-gray-400 w-1/3 justify-center">Photo</TabsTrigger>
+                    <TabsTrigger value="video" className="hs-tab-active:bg-[#ffffff33] hs-tab-active:text-white hs-tab-active:font-semibold py-1 px-4 inline-flex items-center gap-2 bg-transparent text-[14px] font-semibold text-center text-gray-500 rounded-[12px] hover:text-[#80849f] dark:hover:text-gray-400 w-1/3 justify-center">Video</TabsTrigger>
+                  </TabsList>
+                </div>
+                <TabsContent value="news" className="mt-10">
                   <div className="grid grid-cols-12 gap-4">
                     {products.map((product) => (
                       <CardList
@@ -42,7 +49,7 @@ export default function sectionNewsList() {
                     ))}
                   </div>
                 </TabsContent>
-                <TabsContent value="photo">
+                <TabsContent value="photo" className="mt-10">
                   <div className="grid grid-cols-12 gap-4">
                     {products.map((product) => (
                       <CardList
@@ -54,7 +61,7 @@ export default function sectionNewsList() {
                     ))}
                   </div>
                 </TabsContent>
-                <TabsContent value="video">
+                <TabsContent value="video" className="mt-10">
                   <div className="grid grid-cols-12 gap-4">
                     {products.map((product) => (
                       <CardList
